7fb6337968dcd57e77ed3cdf641287ee5035edb8
CosmicLatte009 <ohhm009@gmail.com>
2023-10-05 16:04:12 +0900

    day8 assignment1: checkConnectivity, SmartRefresher, GridView.builder

---
+++ day8/assignment1/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:shimmer/shimmer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  RefreshController refreshController = RefreshController();
+
+  bool isLoading = true;
+  bool isConnected = false;
+  var resultData;
+
+  void initState() async {
+    super.initState();
+    await checkConnection();
+    await getData();
+  }
+
+  Future<void> checkConnection() async {
+    setState(() {
+      isConnected = false;
+    });
+    await Future.delayed(Duration(seconds: 2));
+
+    Connectivity().checkConnectivity();
+
+    setState(() {
+      isConnected = true;
+    });
+  }
+
+  Future<void> getData() async {
+    var dio = Dio();
+    var res = await dio.get("https://sniperfactory.com/sfac/http_day16_dogs");
+
+    resultData = res.data["body"];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("8일차 과제"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await checkConnection();
+        },
+        child: Icon(Icons.wifi_find),
+      ),
+      body: SmartRefresher(
+        controller: refreshController,
+        enablePullDown: true,
+        onRefresh: () async {
+          setState(() {
+            isLoading = true;
+          });
+          await Future.delayed(Duration(seconds: 2));
+
+          setState(() {
+            getData();
+            isLoading = false;
+          });
+          refreshController.refreshCompleted();
+        },
+        child: !isConnected
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("인터넷 확인 중 입니다"),
+                    SizedBox(height: 10),
+                    CircularProgressIndicator()
+                  ],
+                ),
+              )
+            : isLoading == false
+                ? GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: resultData.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(5),
+                              child: Image.network(
+                                resultData[index]["url"],
+                                width: 120,
+                                height: 120,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              resultData[index]["msg"],
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            SizedBox(height: 4),
+                            Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Icon(
+                                  Icons.comment,
+                                  size: 18,
+                                )),
+                          ]),
+                        ),
+                      );
+                    })
+                : GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: resultData.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Shimmer.fromColors(
+                          baseColor: Colors.grey,
+                          highlightColor: Colors.white70,
+                          child: Card());
+                    }),
+      ),
+    ));
+  }
+}
